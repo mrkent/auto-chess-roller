d39d0e1b7ea30c05bad9a888cc0460d4f230ab06
Kent Liu <kentliu@gmail.com>
2019-06-03 17:20:48 -0700

    Updates

---
+++ precache-manifest.e2739455881b40517fb309a3ec3377fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688dbc3d8c1b4008e35940e137062ccb",
+    "url": "/auto-chess-roller/index.html"
+  },
+  {
+    "revision": "567d26a77660168faf0d",
+    "url": "/auto-chess-roller/static/css/main.7c58b315.chunk.css"
+  },
+  {
+    "revision": "c231f2a51ccae7da131e",
+    "url": "/auto-chess-roller/static/js/2.fc34e5b8.chunk.js"
+  },
+  {
+    "revision": "567d26a77660168faf0d",
+    "url": "/auto-chess-roller/static/js/main.deb9047d.chunk.js"
+  },
+  {
+    "revision": "51e9ec83d291b7be1af4",
+    "url": "/auto-chess-roller/static/js/runtime~main.d20d3e2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/auto-chess-roller/static/media/logo.5d5d9eef.svg"
+  }
+]);
